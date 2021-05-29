d6cb9479026417deba123122f4cec4c236e08253
MiniMinja <minyoungheo.heo@gmail.com>
2021-05-29 02:17:41 -0700

    Finished 2015 feb bronze p1 part 1

---
+++ bronze/_2015Feb/censor.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	freopen("censor.in", "r", stdin);
+	freopen("censor.out", "w", stdout);
+
+	string s;
+	cin >> s;
+	string t;
+	cin >> t;
+
+	int foundT = 0;
+	while(1){
+		for(int i = 0;i<=s.length()-t.length();i++){
+			if(s[i] == '\0') continue;
+			int currentMatch = 1;
+			int sj = i;
+			for(int j = 0;j<t.length();j++){
+				while(s[sj] == '\0') sj++;
+				if(s[sj] != t[j]){
+					currentMatch = 0;
+					break;
+				}
+				sj++;
+			}
+			if(currentMatch){
+				for(int j = i;j<sj;j++){
+					s[j] = '\0';
+				}	
+				foundT = 1;
+			}
+		}
+		if(!foundT) break;
+		foundT = 0;
+	}
+
+	for(int i = 0;i<s.length();i++){
+		if(s[i] != '\0') cout << s[i];
+	}
+	cout << endl;
+}
