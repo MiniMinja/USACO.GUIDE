05debbc5ca8173acd58df973a79cd360cfc65b80
MiniMinja <minyoungheo.heo@gmail.com>
2021-07-17 22:01:23 -0700

    2017 open bronze p2 part 3: no memory at all. memory error seems annoying, but no runtime. not sure whats wrong

--- bronze/_2017Open/cownomics.cpp
+++ bronze/_2017Open/cownomics.cpp
@@ -1,70 +1,5 @@
 #include <bits/stdc++.h>
 
-struct triple{
-	char a, b, c;
-
-	triple(char a, char b, char c){
-		this->a = a;
-		this->b = b;
-		this->c = c;
-	}
-
-	std::string getString(){
-		std::string ret;
-		ret += '(';
-		ret += a;
-		ret += ", ";
-		ret += b;
-		ret += ", ";
-		ret += c;
-		ret += ')';
-		return ret;
-	}
-};
-
-struct possible{
-	std::vector<triple*> alltriples;
-
-	int size(){
-		return alltriples.size();
-	}
-};
-
-void printCombos(std::vector<possible*>& ac){
-	for(int i = 0;i<ac.size();i++){
-		std::cout << i << ": ";
-		bool fronted = true;
-		for(int j = 0;j<ac[i]->alltriples.size();j++){
-			if(fronted){
-				fronted = false;
-			}
-			else{
-				std::cout << " ";
-			}
-			std::cout << ac[i]->alltriples[j]->getString();	
-		}
-		std::cout << std::endl;
-	}
-}
-
-bool matches(triple* a, triple* b){
-	return a->a == b->a && a->b == b->b && a->c == b->c;
-}
-
-bool confirms(possible* a, possible* b){
-	for(int i = 0;i<a->size();i++){
-		for(int j = 0;j<b->size();j++){
-			//std::cout << "\tComparing: " << a->alltriples[i]->getString() << " " << b->alltriples[j]->getString() << std::endl;
-			if(matches(a->alltriples[i], b->alltriples[j])){
-				//std::cout << "fail\n" << std::endl;
-				return false;
-			}
-		}
-	}
-	//std::cout << "success\n" << std::endl;
-	return true;
-}
-
 int main(){
 	std::freopen("cownomics.in", "r", stdin);
 	std::freopen("cownomics.out", "w", stdout);
@@ -98,9 +33,7 @@ int main(){
 					if(foundMatch) break;
 					for(int p = 0;p<N;p++){
 						if(foundMatch) break;
-						triple* st = new triple(spotty[s][i], spotty[s][j], spotty[s][k]);
-						triple* pt = new triple(plain[p][i], plain[p][j], plain[p][k]);
-						if(matches(st, pt)){
+						if( (spotty[s][i] == plain[p][i] && spotty[s][j] == plain[s][j] && spotty[s][k] == plain[s][k]) ){
 							foundMatch = true;
 						}
 					}
